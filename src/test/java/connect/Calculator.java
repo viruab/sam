06555b4cd96e17d7bdfdb079d3f1d06331702411
rupesh <rup123@gmail.com>
2020-09-10 00:35:13 +0530

    first

---
+++ src/test/java/connect/Calculator.java
@@ -0,0 +1,16 @@
+package connect;
+
+public class Calculator {
+	static int x=10;
+	static int y=20;
+	static int product=x*y;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(product);
+		
+
+	}
+
+}
