73f398d3f756e60803a9d9024888d3869c898ac9
rupesh <rup123@gmail.com>
2020-09-10 12:10:17 +0530

    second

--- src/main/java/connect/Sum.java
+++ src/main/java/connect/Sum.java
@@ -1,15 +1,10 @@
 package connect;
 
-public class Calculator {
-	
+public class Sum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- int x=10;
-	 int y=20;
-	 int product=x*y;
-		System.out.println(product);
-		
+		System.out.print("fg");
 
 	}
 
